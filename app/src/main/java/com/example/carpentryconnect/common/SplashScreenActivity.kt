88a25fa2de761752f08797bfb1751a6633fd4ec0
Alvin Mwakai <mwakaimwambala@gmail.com>
2024-02-03 22:53:17 +0300

    completed splash screen

--- app/src/main/java/com/example/carpentryconnect/common/SplashScreenActivity.kt
+++ app/src/main/java/com/example/carpentryconnect/common/SplashScreenActivity.kt
@@ -1,10 +1,13 @@
 package com.example.carpentryconnect.common
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AccelerateInterpolator
+import android.widget.ImageView
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -23,10 +26,29 @@ class SplashScreenActivity : AppCompatActivity() {
             .BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
 
+        animateLogo()
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, SignInActivity::class.java))
             finish()
         }, 3000)
+    }
+
+    private fun animateLogo() {
+        val imageView = findViewById<ImageView>(R.id.logo)
+        ObjectAnimator.ofFloat(imageView,
+            "scaleX",
+                    0.5f, 1f)
+            .apply {
+                duration = 1000
+                interpolator = AccelerateInterpolator()
+            }.start()
+        ObjectAnimator.ofFloat(imageView,
+            "scaleY",
+            0.6f, 1f)
+            .apply {
+                duration = 1000
+                interpolator = AccelerateInterpolator()
+            }.start()
     }
 }
